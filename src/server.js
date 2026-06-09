7c6196a433f5de7bfcad80396f066e3b3799c982
Iago Pinheiro <iagoo.pinheiro73@gmail.com>
2026-06-09 15:32:52 -0300

    fix: altera porta padrao para 5000

--- src/server.js
+++ src/server.js
@@ -64,7 +64,7 @@ app.delete('/tasks/:id', (req, res) => {
   res.status(204).send();
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
